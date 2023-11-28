8da10a5b8c5b0958bc80a5c5fb559e963a9fee78
NodeNebula <node.nebula@gmail.com>
2023-11-28 20:45:12 +0100

    2015 Day 7

--- 2015 - Python/advent-of-code-2015-07/2015-07.1.py
+++ 2015 - Python/advent-of-code-2015-07/2015-07.1.py
@@ -1,53 +1,33 @@
-class BobbysBinaryToy:
-    def __init__(self):
-        self.result = dict()
-        self.file = open("test.txt")
+calc = dict()
+results = dict()
 
-        self.getData(self.file)
+for command in open("input.txt"):
+    (ops, res) = command.split('->')
+    calc[res.strip()] = ops.strip().split(' ')
 
-    def computeProcess(self):
-        
+def calculate(name):
+    try:
+        return int(name)
+    except ValueError:
+        pass
 
-    def getData(self, file):
-        for line in file.readlines():
+    if name not in results:
+        ops = calc[name]
+        if len(ops) == 1:
+            res = calculate(ops[0])
+        else:
+            op = ops[-2]
+            if op == 'AND':
+              res = calculate(ops[0]) & calculate(ops[2])
+            elif op == 'OR':
+              res = calculate(ops[0]) | calculate(ops[2])
+            elif op == 'NOT':
+              res = ~calculate(ops[1]) & 0xffff
+            elif op == 'RSHIFT':
+              res = calculate(ops[0]) >> calculate(ops[2])
+            elif op == 'LSHIFT':
+              res = calculate(ops[0]) << calculate(ops[2])
+        results[name] = res
+    return results[name]
 
-
-
-
-
-BobbysBinaryToy()
-
-
-# calc = dict()
-# results = dict()
-#
-# for command in open("input.txt"):
-#     (ops, res) = command.split('->')
-#     calc[res.strip()] = ops.strip().split(' ')
-#
-# def calculate(name):
-#     try:
-#         return int(name)
-#     except ValueError:
-#         pass
-#
-#     if name not in results:
-#         ops = calc[name]
-#         if len(ops) == 1:
-#             res = calculate(ops[0])
-#         else:
-#             op = ops[-2]
-#             if op == 'AND':
-#               res = calculate(ops[0]) & calculate(ops[2])
-#             elif op == 'OR':
-#               res = calculate(ops[0]) | calculate(ops[2])
-#             elif op == 'NOT':
-#               res = ~calculate(ops[1]) & 0xffff
-#             elif op == 'RSHIFT':
-#               res = calculate(ops[0]) >> calculate(ops[2])
-#             elif op == 'LSHIFT':
-#               res = calculate(ops[0]) << calculate(ops[2])
-#         results[name] = res
-#     return results[name]
-#
-# print "a: %d" % calculate('a')
+print("a:", calculate('a'))
---
+++ 2015 - Python/advent-of-code-2015-07/2015-07.2.py
@@ -0,0 +1,33 @@
+calc = dict()
+results = dict()
+
+for command in open("input2.txt"):
+    (ops, res) = command.split('->')
+    calc[res.strip()] = ops.strip().split(' ')
+
+def calculate(name):
+    try:
+        return int(name)
+    except ValueError:
+        pass
+
+    if name not in results:
+        ops = calc[name]
+        if len(ops) == 1:
+            res = calculate(ops[0])
+        else:
+            op = ops[-2]
+            if op == 'AND':
+              res = calculate(ops[0]) & calculate(ops[2])
+            elif op == 'OR':
+              res = calculate(ops[0]) | calculate(ops[2])
+            elif op == 'NOT':
+              res = ~calculate(ops[1]) & 0xffff
+            elif op == 'RSHIFT':
+              res = calculate(ops[0]) >> calculate(ops[2])
+            elif op == 'LSHIFT':
+              res = calculate(ops[0]) << calculate(ops[2])
+        results[name] = res
+    return results[name]
+
+print("a:", calculate('a'))
